d2e3a40cb88acdcb03ac2b9111a30527a31d0b9d
pieterg <pieterg@users.sourceforge.net>
2018-03-26 13:51:44 +0200

    VideoHardware: use 'auto' aspect ratio policy when it is available
    Components/AVSwitch.py adds the 'auto' option to the aspect ratio policy
    setting choices when /proc/stb/video/policy(2)_choices contains 'auto'.
    
    So when 'auto' is selected by the user, we should write 'auto' to the
    hardware in order to select automatic aspect ratio policy mode.
    
    Until now, the option 'bestfit' was written to the hardware, when 'auto'
    was selected, the 'auto' hardware option was never used.
    However, the 'bestfit' hardware option is already available through the
    'scale' policy setting, which clearly is not the same as 'auto' aspect
    ratio policy.

--- lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
+++ lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
@@ -416,25 +416,19 @@ class VideoHardware:
 				aspect = "16:9"
 			else:
 				aspect = {"16_9": "16:9", "16_10": "16:10"}[config.av.aspect.value]
-			policy_choices = {"pillarbox": "panscan", "panscan": "letterbox", "nonlinear": "nonlinear", "scale": "bestfit", "bestfit": "bestfit", "letterbox": "letterbox"}
-			if os.path.exists("/proc/stb/video/policy_choices") and "auto" in open("/proc/stb/video/policy_choices").readline():
-				policy_choices.update({"auto": "auto"})
-			else:
-				policy_choices.update({"auto": "bestfit"})
+			policy_choices = {"pillarbox": "panscan", "panscan": "letterbox", "nonlinear": "nonlinear", "scale": "bestfit", "auto": "auto"}
 			policy = policy_choices[config.av.policy_43.value]
-			policy2_choices = {"letterbox": "letterbox", "panscan": "panscan", "nonlinear": "nonlinear", "scale": "bestfit", "bestfit": "bestfit"}
-			if os.path.exists("/proc/stb/video/policy2_choices") and "auto" in open("/proc/stb/video/policy2_choices").readline():
-				policy2_choices.update({"auto": "auto"})
-			else:
-				policy2_choices.update({"auto": "bestfit"})
-			# print "VIDEOHARDWARE policy2_choices: ", policy2_choices
+			policy2_choices = {"letterbox": "letterbox", "panscan": "panscan", "scale": "bestfit", "auto": "auto"}
 			policy2 = policy2_choices[config.av.policy_169.value]
 		elif is_auto:
 			aspect = "any"
-			policy = "bestfit"
+			if "auto" in config.av.policy_43.choices:
+				policy = "auto"
+			else:
+				policy = "bestfit"
 		else:
 			aspect = "4:3"
-			policy = {"letterbox": "letterbox", "panscan": "panscan", "scale": "bestfit", "auto": "bestfit"}[config.av.policy_169.value]
+			policy = {"letterbox": "letterbox", "panscan": "panscan", "scale": "bestfit", "auto": "auto"}[config.av.policy_169.value]
 
 		if not config.av.wss.value:
 			wss = "auto(4:3_off)"
