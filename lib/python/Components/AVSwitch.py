8963ddbfe235dc7be1278575d1ca5b8aa5d6f269
pieterg <pieterg@users.sourceforge.net>
2018-03-26 14:22:09 +0200

    AVSwitch: update translator comments on 'scale' aspect ratio policy
    'scale' aspect ratio mode does not necessarily break the aspect ratio
    when the display ratio does not match the content ratio. 1:1 scaling is
    more likely to be applied, even though some implementations might choose
    other types of scaling

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -119,10 +119,10 @@ def InitAVSwitch():
 		policy2_choices.update({"nonlinear": _("Stretch nonlinear")})
 
 	if "scale" in policy2_choices_raw and not "auto" in policy2_choices_raw and not "bestfit" in policy2_choices_raw:
-		# TRANSLATORS: (aspect ratio policy: display as fullscreen, with stretching all parts of the picture with the same factor (All parts lose aspect))
+		# TRANSLATORS: (aspect ratio policy: scale as close to fullscreen as possible)
 		policy2_choices.update({"scale": _("Stretch linear")})
 	if "auto" in policy2_choices_raw and not "bestfit" in policy2_choices_raw:
-		# TRANSLATORS: (aspect ratio policy: scale as close to fullscreen as possible)
+		# TRANSLATORS: (aspect ratio policy: automatically select the best aspect ratio mode)
 		policy2_choices.update({"auto": _("Stretch linear")})
 	if "bestfit" in policy2_choices_raw:
 		# TRANSLATORS: (aspect ratio policy: display as fullscreen, with stretching all parts of the picture with the same factor (All parts lose aspect))
@@ -171,10 +171,10 @@ def InitAVSwitch():
 
 	# "auto", "bestfit" and "scale" are aliasses for the same: Stretch linear
 	if "scale" in policy_choices_raw and not "auto" in policy_choices_raw and not "bestfit" in policy_choices_raw:
-		# TRANSLATORS: (aspect ratio policy: display as fullscreen, with stretching all parts of the picture with the same factor (All parts lose aspect))
+		# TRANSLATORS: (aspect ratio policy: scale as close to fullscreen as possible)
 		policy_choices.update({"scale": _("Stretch linear")})
 	if "auto" in policy_choices_raw and not "bestfit" in policy_choices_raw:
-		# TRANSLATORS: (aspect ratio policy: scale as close to fullscreen as possible)
+		# TRANSLATORS: (aspect ratio policy: automatically select the best aspect ratio mode)
 		policy_choices.update({"auto": _("Stretch linear")})
 	if "bestfit" in policy_choices_raw:
 		# TRANSLATORS: (aspect ratio policy: display as fullscreen, with stretching all parts of the picture with the same factor (All parts lose aspect))
