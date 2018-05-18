7b905cb5113da0ae2a2550c49c1a2f8b8b24c226
teamblue-e2 <teamblue@email.de>
2018-05-18 23:41:27 +0200

    Fallbacktimer latest changes - thx openpli

--- lib/python/Screens/TimerEntry.py
+++ lib/python/Screens/TimerEntry.py
@@ -26,14 +26,14 @@ from datetime import datetime
 import urllib
 
 class TimerEntry(Screen, ConfigListScreen):
-	def __init__(self, session, timer, edit=False):
+	def __init__(self, session, timer):
 		Screen.__init__(self, session)
 		self.timer = timer
 
-		self.edit = edit
-		self.service_ref_prev = self.timer.service_ref
-		self.begin_prev = self.timer.begin
-		self.end_prev = self.timer.end
+		self.timer.service_ref_prev = self.timer.service_ref
+		self.timer.begin_prev = self.timer.begin
+		self.timer.end_prev = self.timer.end
+		self.timer.external_prev = self.timer.external
 
 		self.entryDate = None
 		self.entryService = None
@@ -164,7 +164,7 @@ class TimerEntry(Screen, ConfigListScreen):
 	def createSetup(self, widget):
 		self.list = []
 		self.entryRemoteTimer = getConfigListEntry(_("Remote Timer"), self.timerentry_fallback)
-		if config.usage.remote_fallback_external_timer.value and config.usage.remote_fallback.value and not hasattr(self, "timerentry_remote") and not self.edit:
+		if config.usage.remote_fallback_external_timer.value and config.usage.remote_fallback.value and not hasattr(self, "timerentry_remote"):
 			self.list.append(self.entryRemoteTimer)
 		self.entryName = getConfigListEntry(_("Name"), self.timerentry_name)
 		self.list.append(self.entryName)
@@ -523,11 +523,8 @@ class TimerEntry(Screen, ConfigListScreen):
 	def saveTimer(self):
 		self.session.nav.RecordTimer.saveTimer()
 
-	def keyCancel(self, answer=True, message=""):
-		if answer:
-			self.close((False,))
-		else:
-			print "[TimerEntry] keyCancel something went wrong with fallback timer", message
+	def keyCancel(self):
+		self.close((False,))
 
 	def pathSelected(self, res):
 		if res is not None:
